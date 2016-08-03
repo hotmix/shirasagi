0de289bf0fa18e8b94d345d5722357931822cd25
hotmix <hotmix.blog@gmail.com>
2016-08-03 19:27:38 +0900

    modified spec/facility/agents/nodes/page_spec coverage 45

--- spec/features/facility/agents/nodes/page_spec.rb
+++ spec/features/facility/agents/nodes/page_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe "facility_agents_nodes_page", type: :feature, dbscope: :example do
-  let(:site)   { cms_site }
-  let(:layout) { create_cms_layout }
-  let(:node)   { create :cms_node_page, layout_id: layout.id, filename: "node" }
+  let(:site) {create :cms_site}
+  let(:node) { create :facility_node_page }
 
   context "public" do
-    let!(:item) { create :cms_page, filename: "node/item" }
 
     before do
       Capybara.app_host = "http://#{site.domain}"
@@ -15,38 +13,20 @@ describe "facility_agents_nodes_page", type: :feature, dbscope: :example do
     it "#index" do
       visit node.url
       expect(status_code).to eq 200
-      expect(page).to have_css(".pages")
-      expect(page).to have_selector("article")
+      expect(page).to have_css(".facility_page")
+
     end
 
     it "#index with kana" do
       visit node.url.sub('/', SS.config.kana.location + '/')
       expect(status_code).to eq 200
-      expect(page).to have_css(".pages")
-      expect(page).to have_selector("article")
-      expect(page).to have_selector("a[href='/node/item.html']")
+      expect(page).to have_css(".facility_page")
     end
 
     it "#index with mobile" do
       visit node.url.sub('/', site.mobile_location + '/')
       expect(status_code).to eq 200
-      expect(page).to have_css(".pages")
-      expect(page).to have_selector(".tag-article")
-      expect(page).to have_selector("a[href='/mobile/node/item.html']")
-    end
-
-    it "#image_pages" do
-      visit node.url
-      expect(status_code).to eq 200
-      expect(page).to have_css(".pages")
-      expect(page).to have_selector("article")
-    end
-
-    it "#map_pages" do
-      visit node.url
-      expect(status_code).to eq 200
-      expect(page).to have_css(".pages")
-      expect(page).to have_selector("article")
+      expect(page).to have_css(".facility_page")
     end
 
   end
